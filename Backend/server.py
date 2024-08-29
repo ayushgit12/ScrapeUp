f49bc782dea74824d947bb48f19d1c0963ecc997
ayushgit12 <ayush2005raj@gmail.com>
2024-08-29 21:46:23 +0530

    changed host from localhost to 0.0.0.0

--- Backend/server.py
+++ Backend/server.py
@@ -22,4 +22,4 @@ def refresh_prices():
      return refresh_prices_here()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
